a7066c280f7f5a4ac117adaaac8edf5585be01fc
dongsinhho <you@example.com>
2022-05-28 14:47:17 +0700

    fix db

--- server.js
+++ server.js
@@ -4,30 +4,21 @@ import cors from 'cors'
 import { Low } from 'lowdb'
 import { JSONFile } from 'lowdb'
 import fetch from "node-fetch"
+import {join, dirname} from 'path'
+import { fileURLToPath } from 'url'
+import lodash from 'lodash'
 
-const db = new Low(new JSONFile('file.json'))
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const file = join(__dirname, 'file.json')
+const db = new Low(new JSONFile(file))
 
 const app = express()
 const PORT = 8088
-const SERVER = '54.169.182.170:3000'
-const STATION_ID = "áđálkạds"
-var token
-setInterval(() => {
-    fetch(SERVER, {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        redirect: 'follow',
-        credentials: 'include'
-    }).then(
-        response => response.json()
-    ).then(
-        data => token = data.token
-    )
-}, 360000000)
-console.log(token)
+const SERVER = 'http://54.169.182.170:3000'
+const STATION_ID = "6237fa2c1651303e209c7945"
+const email = "stationbinhthanh@gmail.com"
+const password = "Stationbinhthanh123"
+var token = null
 
 app.use(cors({
     "origin": true,
@@ -35,18 +26,38 @@ app.use(cors({
 }))
 app.set('view engine', 'ejs');
 
-// - Quản lý các xe trong trạm với lowDB
-// Nhiệm vụ của station:
-// - login vào system để nhận token
-
-// - tạo một trang web để người dùng có thể truy cập, nhập id và trả xe
-// ==> kiểm tra
-// - nhận request từ hệ thống -> mở khoá xe
-
 app.use(cookieParser())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 
+
+const retrieveToken = async () => {
+    try {
+        const res = await fetch(`${SERVER}/api/users/stationLogin`, {
+            body: JSON.stringify({
+                email: email,
+                password: password
+            }),
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            redirect: 'follow',
+            credentials: 'include'
+        })
+        const data = await res.json()
+        if (data) {
+            token = data.token
+            return true
+        }
+        return false
+    }
+    catch {
+        console.log(res)
+        return false
+    }
+}
 
 app.get('/get_all_slot', async (req, res) => {
     res.status(200).json(db.data.slot)
@@ -64,14 +75,15 @@ app.post('/create_slot', async (req, res) => {
 
 app.get('/bike/:bikeId', async (req, res) => {
     try {
-        db.get('slot')
+        const updateSlot = db.chain.get('slot')
             .find({ bikeId: req.params.bikeId })
             .assign({ bikeId: null, isOpen: true })
-            .write()
-        res.status(200).json({status: true, message: "create success" })
+            .value()
+        db.write(updateSlot);
+        res.status(200).json({ status: true, message: "create success" })
     }
-    catch(e) {
-        res.status(500).json({status: false, message: e})
+    catch (e) {
+        res.status(500).json({ status: false, message: e })
         return
     }
 })
@@ -83,43 +95,64 @@ app.get('/return_bike', async (req, res) => {
     res.render('returnBike.ejs');
 })
 
-app.post('/return_bike', (req, res) => {
+const checkBikeInfo = async (bikeId) => {
+    try {
+        console.log("check bike info")
+        console.log(token)
+        const response = await fetch(`${SERVER}/api/stations/user/${STATION_ID}/${bikeId}`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            redirect: 'follow',
+            credentials: 'include'
+        })
+        console.log(response)
+        if (response.status == 200) {
+            return true
+        } else {
+            return false
+        }
+    } catch(err) {
+        console.log(err)
+        return false
+    }
+}
+
+app.post('/return_bike', async (req, res) => {
     const { bikeId } = req.body
-    fetch(`${SERVER}/${STATION_ID}/${bikeId}`, {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        redirect: 'follow',
-        credentials: 'include'
-    }).then(response => response.json())
-    .then(data => {
-        if(data.status) {
-            db.get('slot')
-                .find({bikeId: null})
-                .assign({ bikeId: bikeId, isOpen: false })
-                .write()
-            return res.render('success.ejs');
+    if (!token) {
+        const result = await retrieveToken()
+        if (!result) {
+            return res.render('failure.ejs', { data: `Nguyên nhân: Xác thực máy chủ không thành công` });
         }
-        else {
-            let data = `Nguyên nhân: ${data.message}`
-            return res.render('failure.ejs', {data: data});
-        }
-    })
-    .catch(e => {
-        let data = `Nguyên nhân: ${e}`
-        return res.render('failure.ejs', {data: data});
-    })     
+    }
+    const check = await checkBikeInfo(bikeId)
+    console.log(check)
+    if (check) {
+        const updateSlot = db.chain.get('slot')
+            .find({ bikeId: null })
+            .assign({ bikeId: bikeId, isOpen: false })
+            .value()
+        db.write(updateSlot)
+        return res.render('success.ejs');
+    } else {
+        return res.render('failure.ejs', { data: `Nguyên nhân: trả xe không thành công` });
+    }
 })
 
 app.delete('/:bikeId', async (req, res) => {
     res.status(200).json({ message: "create success" })
 })
 
-db.read()
+await db.read()
 db.data ||= { slot: [] }
-const { slot } = db.data
+db.chain = lodash.chain(db.data)
+let { slot } = db.data
+
+console.log(slot)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
